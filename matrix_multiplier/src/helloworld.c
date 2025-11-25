1176760b50147382b84f1d1ccc563a036622b9f8
Danial-Changez <danial.changez@gmail.com>
2025-11-25 12:14:10 -0500

    Updating timing to extract average results for consistency

--- matrix_multiplier/src/helloworld.c
+++ matrix_multiplier/src/helloworld.c
@@ -23,7 +23,7 @@
 #include "xiltimer.h"
 
 #define baseaddress XPAR_MATRIX_MULTIPLIER_0_BASEADDR
-
+#define N 1000
 uint32_t pack(int a1, int a2, int a3) {
     return (uint32_t)a1 + ((uint32_t)a2 << 8) + ((uint32_t)a3 << 16);
 }
@@ -34,28 +34,31 @@ int main()
 
     uint32_t *ba_p = (uint32_t *)baseaddress;
 
-    XTime start, end;
-    double time_seconds;
-    unsigned long long time_cycles;
+    uint64_t time_ns;
+    uint64_t sum_cycles = 0;
 
     // Loading input registers
     //  Input matrix A
-    *(ba_p+0) = pack(3,1,4);
-    *(ba_p+1) = pack(1,5,9);
-    *(ba_p+2) = pack(2,6,5);
+    *(ba_p+0) = pack(1,2,3);
+    *(ba_p+1) = pack(4,5,6);
+    *(ba_p+2) = pack(7,8,9);
 
     //  Input matrix B
-    *(ba_p+3) = pack(2,7,1);
-    *(ba_p+4) = pack(8,2,8);
-    *(ba_p+5) = pack(1,8,2);
+    *(ba_p+3) = pack(9,8,7);
+    *(ba_p+4) = pack(6,5,4);
+    *(ba_p+5) = pack(3,2,1);
 
-    // Time register write
-    XTime_GetTime(&start);
-    volatile uint32_t dummy_read = *(ba_p+6); 
-    XTime_GetTime(&end);
+    // Average register write time
+    for (int i = 0; i < N; i++) {
+        XTime start, end;
+        XTime_GetTime(&start);
+        volatile uint32_t dummy_read = *(ba_p+6); 
+        XTime_GetTime(&end);
+        sum_cycles += (end - start);
+    }
     
-    time_cycles = end - start;
-    time_seconds = (double)time_cycles / (double)COUNTS_PER_SECOND;
+    uint64_t avg_cycles = sum_cycles / N;
+    uint64_t avg_ns = avg_cycles * 1000000000ULL / COUNTS_PER_SECOND;
 
     xil_printf("\n\rInput Registers A (Hex):\n\r");
     xil_printf("[ 0x%08x ]\n\r", *(ba_p+0));
@@ -77,8 +80,8 @@ int main()
     xil_printf("[ %05d %05d %05d ]\n\r", (int)(*(ba_p+9)), (int)(*(ba_p+10)), (int)(*(ba_p+11)));
     xil_printf("[ %05d %05d %05d ]\n\r", (int)(*(ba_p+12)), (int)(*(ba_p+13)), (int)(*(ba_p+14)));
 
-    xil_printf("Time to run AXI4 peripheral: %d seconds", time_seconds);
-    xil_printf("Cycles to run AXI4 peripheral: %d cycles", time_cycles);
+    xil_printf("\n\rAverage cycles over %d runs: %llu", N, avg_cycles);
+    xil_printf("\n\rAverage time: %llu ns", avg_ns);
     cleanup_platform();
     return 0;
 }
